38f5315ffe2e1672b4c3154887de8d9436526748
Kristian Rasmussen <kristian.rasmussen@altibox.no>
2017-04-13 00:53:09 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import Player from './components/player/Main'
+
+ReactDOM.render(
+  <div>
+  	<App />
+    <Player />
+  </div>,
+  document.getElementById('root')
+);
